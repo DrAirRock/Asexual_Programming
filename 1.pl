696593dee0d146a4779f5ef2a3c1ef28d5be9d3b
Eric Songefest <ericsognefest@gmail.com>
2017-05-12 07:10:16 -0400

    13 to 9

--- 1.pl
+++ 1.pl
@@ -2,12 +2,8 @@ use File::Slurp;
 @file_path = <./*.pl>;
 foreach $file (@file_path){ 	
 	$temp = $file;
-	$preface = "./";
-	$temp =~ s/\.pl//gi; 
-	$temp =~ s/\.\///gi;
-	$file_out= $temp + 1;
-	$file_out = $file_out . ".pl"; 
-	$file_out = $preface . $file_out;	
+	$temp =~ s/(\.pl)|(\.\/)//gi; 
+	$file_out = "./" . (++$temp) . ".pl"; 
 	$file_info = read_file($file); 
 	write_file($file_out, $file_info); 
 }
