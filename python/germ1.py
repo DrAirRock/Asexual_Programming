645a06d9a6a6e6124a3149f1f2ba9bef0b87dcda
Eric Songefest <ericsognefest@gmail.com>
2017-05-14 23:36:00 -0400

    add python version

---
+++ python/germ1.py
@@ -0,0 +1,7 @@
+payload = ['print' , "'"'payload ='"'" , ',' , '\n' , 'print' , 'payload' ,'\n' , 'for' , 'element' , 'in', 'payload:', '\n\t' , 'print' , 'element,'  ] 
+print 'payload = ', 
+print payload 
+for element in payload: 
+        print element, 
+
+
